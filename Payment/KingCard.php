ca27eb3bddc4e315aaf022c0214636e27960253e
Luu Truong <truonglv@outlook.com>
2020-02-13 14:34:36 +0700

    Fixed invalid log type.

--- Payment/KingCard.php
+++ Payment/KingCard.php
@@ -292,6 +292,9 @@ class KingCard extends AbstractProvider
 
         $mrcOrderId = isset($order['mrc_order_id']) ? $order['mrc_order_id'] : null;
         if (!$mrcOrderId || $mrcOrderId !== $state->requestKey) {
+            $state->logType = 'error';
+            $state->logMessage = 'Mismatch order ID!';
+
             return false;
         }
 
